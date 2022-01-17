90cc0361780b57243c79a0b27a4e0d09a54cb34c
Emehado <emehado@gmail.com>
2022-01-17 14:40:33 +0000

    Build card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,31 @@ import React from "react";
 import { IconContext } from "react-icons";
 import iconsConfig from "./config/icons";
 import Button from "./components/Button";
+import Card from "./components/Card";
 import { IoMdArrowRoundForward, IoMdArrowRoundBack } from "react-icons/io";
 
 function App() {
   return (
     <IconContext.Provider value={iconsConfig}>
-      <div style={{ background: "tomato" }}>
-        <Button>
-          <IoMdArrowRoundBack />
-          Back
-        </Button>
-        <Button primary>
-          Next <IoMdArrowRoundForward />
-        </Button>
+      <div
+        style={{
+          display: "flex",
+          gap: "2rem",
+          justifyContent: "space-between",
+          height: 500,
+          background: "brown",
+          padding: "2rem",
+        }}
+      >
+        <div style={{ flex: 1 }}>
+          <Card>
+            <h1>This is the card heading</h1>
+            <Button>
+              <IoMdArrowRoundBack />
+              Back
+            </Button>
+          </Card>
+        </div>
       </div>
     </IconContext.Provider>
   );
